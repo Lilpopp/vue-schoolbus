0f7a1486c978b684e86a5859231ffc3e8faa64b0
拓海 <2457968197@qq.com>
2022-12-12 20:18:48 +0800

    完成网络请求接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,69 +1,296 @@
 import request from '../utils/request';
-import axios from "axios";
 import path from "./path"
+
+/**
+ * 用户模块
+ */
 //登陆
-export const login = (username,password) => {
+export const login = (username, password) => {
     return request({
         dataType: "json",
-        url: path.user+"/login",
+        url: path.user + "/login",
         method: 'post',
         data: {
-            "username":username,
-            "password":password
+            "username": username,
+            "password": password
         }
     });
 };
 //注册
-export const register = (username,password,sex,phone) => {
+export const register = (username, password, sex, phone) => {
     return request({
         dataType: "json",
-        url: path.user+"/register",
+        url: path.user + "/register",
         method: 'post',
         data: {
-            "username":username,
-            "password":password,
-            "sex":sex,
-            "phone":phone,
-            "is_super":false
+            "username": username,
+            "password": password,
+            "sex": sex,
+            "phone": phone,
+            "is_super": false
         }
     });
 };
 //修改信息
-export const userChange = (password,sex,phone,comment) => {
+export const userChange = (password, sex, phone, comment) => {
     return request({
         dataType: "json",
-        url: path.user+"/change",
+        url: path.user + "/change",
         method: 'post',
         data: {
-            "password":password,
-            "sex":sex,
-            "phone":phone,
-            "comment":comment
+            "password": password,
+            "sex": sex,
+            "phone": phone,
+            "comment": comment
         }
     });
 };
 //查看一个用户的信息
 export const findOneUser = () => {
     return request({
-        url: path.user+"/user",
+        url: path.user + "/user",
         method: 'get'
     });
 };
 //查看用户列表
-export const findUser = (pageNum,pageSize,userName) => {
+export const findUser = (pageNum, pageSize, userName) => {
     return request({
         dataType: "json",
-        url: path.user+"/all",
+        url: path.user + "/all",
         method: 'post',
         data: {
-            "page_num":pageNum,
-            "page_size":pageSize,
-            "user_name":userName
+            "page_num": pageNum,
+            "page_size": pageSize,
+            "user_name": userName
         }
     });
 };
-
-
+/**
+ * 校车模块
+ */
+//添加校车
+export const addBus = (busName, busPrice, busAllNum) => {
+    return request({
+        dataType: "json",
+        url: path.bus + "/add",
+        method: 'post',
+        data: {
+            "bus_name": busName,
+            "bus_price": busPrice,
+            "bus_all_num": busAllNum
+        }
+    });
+}
+//修改校车
+export const changeBus = (busId, busName, busState, busPrice, busAllNum, busNum) => {
+    return request({
+        dataType: "json",
+        url: path.bus + "/change",
+        method: 'post',
+        data: {
+            "bus_id": busName,
+            "bus_name": busPrice,
+            "bus_state": busAllNum,
+            "bus_price": busPrice,
+            "bus_all_num": busAllNum,
+            "bus_num": busNum
+        }
+    });
+}
+//获得校车列表
+export const allBus = (pageNum, pageSize, busName) => {
+    return request({
+        dataType: "json",
+        url: path.bus + "/all",
+        method: 'post',
+        data: {
+            "page_num": pageNum,
+            "page_size": pageSize,
+            "bus_name": busName
+        }
+    });
+}
+//获得单个校车信息
+export const oneBus = busId => {
+    return request({
+        url: path.bus + "/bus/" + busId,
+        method: 'get',
+    });
+}
+//删除校车
+export const deleteBus = busId => {
+    return request({
+        dataType: "json",
+        url: path.bus + "/delete",
+        method: 'post',
+        data: {
+            "bus_id": busId
+        }
+    });
+}
+/**
+ * 路径模块
+ */
+//添加路径
+export const addRoute = (beginSite, endSite, pathwaySite) => {
+    return request({
+        dataType: "json",
+        url: path.route + "/add",
+        method: 'post',
+        data: {
+            "begin_site": beginSite,
+            "end_site": endSite,
+            "pathway_site": pathwaySite
+        }
+    });
+}
+//获取路径 startSite,endSite为空就是不搜索
+export const allRoute = (pageNum, pageSize, startSite, endSite) => {
+    return request({
+        dataType: "json",
+        url: path.route + "/all",
+        method: 'post',
+        data: {
+            "page_num": pageNum,
+            "page_size": pageSize,
+            "start_site": startSite,
+            "end_site": endSite
+        }
+    });
+}
+//更改路径
+export const changeRoute = (routeId, beginSite, endSite, pathwaySite) => {
+    return request({
+        dataType: "json",
+        url: path.route + "/change",
+        method: 'post',
+        data: {
+            "route_id": routeId,
+            "begin_site": beginSite,
+            "end_site": endSite,
+            "pathway_site": pathwaySite
+        }
+    });
+}
+//删除路径
+export const deleteRoute = (routeId) => {
+    return request({
+        dataType: "json",
+        url: path.route + "/delete",
+        method: 'post',
+        data: {
+            "route_id": routeId
+        }
+    });
+}
+/**
+ * 发车线路/正在运行车辆线路 模块
+ */
+//添加发车线路
+export const addSchedule = (beginSite, endSite, routeId, beginTime, busId) => {
+    return request({
+        dataType: "json",
+        url: path.schedule + "/add",
+        method: 'post',
+        data: {
+            "begin_site": beginSite,
+            "end_site": endSite,
+            "route_id": routeId,
+            "begin_time": beginTime,
+            "bus_id": busId
+        }
+    });
+}
+//改变发车路线
+export const changeSchedule = (scheduleId, beginSite, endSite, routeId, beginTime, busId) => {
+    return request({
+        dataType: "json",
+        url: path.schedule + "/change",
+        method: 'post',
+        data: {
+            "schedule_id": scheduleId,
+            "begin_site": beginSite,
+            "end_site": endSite,
+            "route_id": routeId,
+            "begin_time": beginTime,
+            "bus_id": busId
+        }
+    });
+}
+//查看所有的发车线路
+export const allSchedule = (pageNum, pageSize, beginSite, endSite, busName) => {
+    return request({
+        dataType: "json",
+        url: path.schedule + "/all",
+        method: 'post',
+        data: {
+            "page_num": pageNum,
+            "page_size": pageSize,
+            "begin_site": beginSite,
+            "end_site": endSite,
+            "bus_name": busName
+        }
+    });
+}
+//删除发车路线
+export const deleteSchedule = (scheduleId) => {
+    return request({
+        dataType: "json",
+        url: path.schedule + "/delete",
+        method: 'post',
+        data: {
+            "schedule_id": scheduleId,
+        }
+    });
+}
+/**
+ * 预定模块
+ */
+//预定
+export const addOrder = (scheduleId) => {
+    return request({
+        dataType: "json",
+        url: path.order + "/add",
+        method: 'post',
+        data: {
+            "schedule_id": scheduleId,
+        }
+    });
+}
+//支付
+export const payOrder = (orderId) => {
+    return request({
+        dataType: "json",
+        url: path.order + "/pay",
+        method: 'post',
+        data: {
+            "order_id": orderId,
+        }
+    });
+}
+//查看预定信息
+export const allOrder = (pageNum, pageSize) => {
+    return request({
+        dataType: "json",
+        url: path.order + "/all",
+        method: 'post',
+        data: {
+            "page_num": pageNum,
+            "page_size": pageSize
+        }
+    });
+}
+//删除预定信息
+export const deleteOrder = (orderId) => {
+    return request({
+        dataType: "json",
+        url: path.order + "/delete",
+        method: 'post',
+        data: {
+            "order_id": orderId
+        }
+    });
+}
 export const fetchData = query => {
     return request({
         url: './table.json',
@@ -72,7 +299,7 @@ export const fetchData = query => {
     });
 };
 
-export const insertData = (data,path) => {
+export const insertData = (data, path) => {
     return request({
         url: 'http://localhost:8080' + path,
         method: 'post',
@@ -80,7 +307,7 @@ export const insertData = (data,path) => {
     })
 }
 
-export const insertDataParam = (data,path) => {
+export const insertDataParam = (data, path) => {
     return request({
         url: 'http://localhost:8080' + path,
         method: 'post',
@@ -88,7 +315,7 @@ export const insertDataParam = (data,path) => {
     })
 }
 
-export const deleteData = (data,path) => {
+export const deleteData = (data, path) => {
     return request({
         url: 'http://localhost:8080' + path,
         method: 'post',
@@ -96,7 +323,7 @@ export const deleteData = (data,path) => {
     })
 }
 
-export const updateData = (data ,path) => {
+export const updateData = (data, path) => {
     return request({
         url: 'http://localhost:8080' + path,
         method: 'post',
--- src/api/path.js
+++ src/api/path.js
@@ -2,6 +2,10 @@
 const baseUrl = "http://124.222.186.96:2020/API"
 const base = {
     baseUrl:baseUrl,
-    user:baseUrl+"/user"
+    user:baseUrl+"/user",
+    bus:baseUrl+"/add",
+    route:baseUrl+"/route",
+    schedule:baseUrl+"/schedule",
+    order:baseUrl+"/order",
 }
 export default base
